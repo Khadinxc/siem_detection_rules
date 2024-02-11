1d2f8cd0ec0042934ddbf474cd3cf10c2689c376
kaiberxc <89855993+Khadinxc@users.noreply.github.com>
2024-02-11 12:44:42 +1100

    testing elastic sync action

--- development/update_alert.py
+++ development/update_alert.py
@@ -3,7 +3,6 @@ import os
 import tomllib
 
 #script updates alerts from filepath with a api put request
-
 url = "https://591f836b7e854b68a51f2db3db3999df.us-central1.gcp.cloud.es.io:9243/api/detection_engine/rules"
 api_key = os.environ['ELASTIC_KEY']
 headers = {
@@ -12,46 +11,50 @@ headers = {
     'Authorization': 'ApiKey ' + api_key
 }
 
+changed_files = os.environ["CHANGED_FILES"]
+
 data = ""
 
 for root, dirs, files in os.walk("elastic_rules"): 
     for file in files:
-        data = "{\n"
-        if file.endswith(".toml"):
-            full_path = os.path.join(root, file)
-            with open(full_path,"rb") as toml:
-                alert = tomllib.load(toml)
+        if file in changed_files:
+            #if "excessive_web_traffic.toml" in "detections/excessive_web_traffic.toml"
+            data = "{\n"
+            if file.endswith(".toml"):
+                full_path = os.path.join(root, file)
+                with open(full_path,"rb") as toml:
+                    alert = tomllib.load(toml)
 
-            if alert['rule']['type'] == 'query': #query based alert
-                required_fields = ['author','description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query','threat']
-            elif alert['rule']['type'] == 'eql': #event correlation alert
-                required_fields = ['author','description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'language','threat']
-            elif alert['rule']['type'] == 'threshold': #threshold based alert
-                required_fields = ['author','description','name','rule_id','risk_score','severity','type','query','threshold','threat']
-            else:
-                print("Unsupported rule type found in: " + full_path)
-                break
+                if alert['rule']['type'] == 'query': #query based alert
+                    required_fields = ['author','description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query','threat']
+                elif alert['rule']['type'] == 'eql': #event correlation alert
+                    required_fields = ['author','description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'language','threat']
+                elif alert['rule']['type'] == 'threshold': #threshold based alert
+                    required_fields = ['author','description','name','rule_id','risk_score','severity','type','query','threshold','threat']
+                else:
+                    print("Unsupported rule type found in: " + full_path)
+                    break
 
-            for field in alert['rule']:
-                if field in required_fields:
-                    if type(alert['rule'][field]) == list:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
-                    elif type(alert['rule'][field]) == str:
-                        if field == 'description' :
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
-                        elif field == 'query' :
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"                            
-                        else:
-                            data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
-                    elif type(alert['rule'][field]) == int:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + "," + "\n"
-                    elif type(alert['rule'][field]) == dict:
-                        data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
-                                                
-            data += "   \"enabled\": true\n}"
+                for field in alert['rule']:
+                    if field in required_fields:
+                        if type(alert['rule'][field]) == list:
+                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
+                        elif type(alert['rule'][field]) == str:
+                            if field == 'description' :
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"").replace("\\","\\\\") + "\"," + "\n"
+                            elif field == 'query' :
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\\","\\\\").replace("\"","\\\"").replace("\n"," ") + "\"," + "\n"                            
+                            else:
+                                data += "  " + "\"" + field + "\": \"" + str(alert['rule'][field]).replace("\n"," ").replace("\"","\\\"") + "\"," + "\n"
+                        elif type(alert['rule'][field]) == int:
+                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]) + "," + "\n"
+                        elif type(alert['rule'][field]) == dict:
+                            data += "  " + "\"" + field + "\": " + str(alert['rule'][field]).replace("'","\"") + "," + "\n"
+                                                    
+                data += "   \"enabled\": true\n}"
 
-        rule_id = (alert['rule']['rule_id'])
-        url = url + "?rule_id=" + rule_id
+            rule_id = (alert['rule']['rule_id'])
+            url = url + "?rule_id=" + rule_id
 
         # print(data)
         elastic_data = requests.put(url, headers=headers, data=data).json()
